d4f14526ea911fea3b00ed819cc49eb3ce3c2758
Irfan140 <irfanmehmud140@gmail.com>
2025-11-07 23:31:30 +0530

    refactor

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,18 +3,13 @@ import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 
+app.use(express.json());
 app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
-  res.json({
-    message: "API running fine",
+  res.status(200).json({
+    message: "API hit",
   });
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
 });
 
 export default app;
