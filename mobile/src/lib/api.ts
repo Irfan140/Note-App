924204297f80fefaba94a83c933f9b1dce49b708
Irfan <irfanmehmud140@gmail.com>
2026-01-30 12:29:55 +0530

    fix: axios related error

--- mobile/src/lib/api.ts
+++ mobile/src/lib/api.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useAuth } from "@clerk/clerk-expo";
 
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
+
 export const useApi = () => {
   const { getToken } = useAuth();
 
   const api = axios.create({
-    baseURL: "http://192.168.1.69:5000",
+    baseURL: API_URL,
   });
 
   api.interceptors.request.use(async (config) => {
