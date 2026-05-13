a4cac963824b6ce3a846ec5f125b26164b2604fb
Irfan140 <irfanmehmud140@gmail.com>
2026-05-14 00:51:04 +0530

    feat: add .env.example and segregate environment for development and deployment

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,10 @@
 import app from "./app";
+import dotenv from "dotenv"
+
+// for correctly loading .env files in development and production environments
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`
+})
 
 const PORT = parseInt(process.env.PORT ?? "3000", 10);
 
